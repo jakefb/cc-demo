f6ce9c948bb3a203381e82eb6793cde16e5dbffd
Jake Fulton <jake@jakefulton.com>
2022-03-24 09:55:51 +1300

    Refactor modal.js so it initializes modals individually using a passed modalButton element, rather than searching for data-modal button elements on the page and initializing them all - this approach allows a callback function to be specified per modal

--- src/app.js
+++ src/app.js
@@ -1,3 +1,13 @@
-import { initializeModals } from './modal-component/modal.js'
+import { initializeModal } from './modal-component/modal.js'
 
-initializeModals({ appRoot: document.querySelector('#app') })
+function updateMainPageText ({ result }) {
+  const resultElement = document.querySelector('#result')
+
+  if (result === 'yes') {
+    resultElement.textContent = 'You just clicked "Yes"'
+  } else if (result === 'cancel') {
+    resultElement.textContent = 'You just clicked "Cancel"'
+  }
+}
+
+initializeModal({ modalButton: document.querySelector('#confirmationModal'), callback: updateMainPageText })
--- src/modal-component/modal.js
+++ src/modal-component/modal.js
@@ -1,35 +1,66 @@
-function initializeModals ({ appRoot = document }) {
-  appRoot.querySelectorAll('button[data-modal-button')
-    .forEach(
-      element => {
-        const { modalMessage } = element.dataset
-          
-        if (modalMessage) {
-          const dialogElement = document.createElement('div')
-          dialogElement.className = 'modal'
-          dialogElement.style.display = 'none'
-
-          const dialogElementInner = document.createElement('div')
-          dialogElementInner.className = 'modal-inner'
-          dialogElementInner.innerHTML = `
-            <p>${modalMessage}</p>
-            <div>
-              <button>Yes</button>
-              <button>Cancel</button>
-            </div>
-          `
-
-          dialogElement.append(dialogElementInner)
-          document.body.append(dialogElement)
-
-          element.onclick = () => {
-            dialogElement.style.display = 'flex'
-          }
-        } else {
-          throw Error('Please provide a message for the modal, using the "data-modal-message" HTML attriture on the button element:', element)
-        }
-      }
-    )
+function openModal (dialogElement) {
+  dialogElement.style.display = 'flex'
 }
 
-export { initializeModals }
+function closeModal (dialogElement) {
+  dialogElement.style.display = 'none'
+}
+
+function initializeModal ({ modalButton, callback }) {
+  if (!modalButton) {
+    throw Error('Please pass a modalButton element to initializeModal (as a property argument)')
+  }
+  if (typeof callback !== 'function') {
+    throw Error('Please provide a callback function for the modal, using the "data-modal-callback" HTML attribute on the button element:', modalButton)
+  }
+
+  const { modalMessage } = modalButton.dataset
+
+  if (modalMessage) {
+    const dialogElement = document.createElement('div')
+    dialogElement.className = 'modal'
+    dialogElement.style.display = 'none'
+
+    const dialogElementInner = document.createElement('div')
+    dialogElementInner.className = 'modal-inner'
+    dialogElementInner.innerHTML = `
+      <p>${modalMessage}</p>
+    `
+
+    const buttonWrapperDiv = document.createElement('div')
+
+    const yesButton = document.createElement('button')
+    yesButton.textContent = 'Yes'
+    yesButton.onclick = () => {
+      console.log('clicked yes')
+      callback({ result: 'yes' })
+      closeModal(dialogElement)
+    }
+
+    const cancelButton = document.createElement('button')
+    cancelButton.textContent = 'Cancel'
+    cancelButton.onclick = () => {
+      console.log('clicked cancel')
+      callback({ result: 'cancel' })
+      closeModal(dialogElement)
+    }
+
+    buttonWrapperDiv.append(yesButton)
+    buttonWrapperDiv.append(cancelButton)
+
+    dialogElementInner.append(buttonWrapperDiv)
+
+    dialogElement.append(dialogElementInner)
+    document.body.append(dialogElement)
+
+    modalButton.onclick = () => {
+      openModal(dialogElement)
+    }
+  } else {
+    if (!modalMessage) {
+      throw Error('Please provide a message for the modal, using the "data-modal-message" HTML attribute on the button element:', element)
+    }
+  }
+}
+
+export { initializeModal }
